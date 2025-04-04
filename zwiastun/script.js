9a2f02aef44071e1721f96803bd14ce414fc73dd
lontem2 <lontem2@wp.pl>
2025-04-04 18:53:50 +0200

    update

--- zwiastun/script.js
+++ zwiastun/script.js
@@ -1,3 +1,18 @@
 function showTrailer() {
-    alert("Zwiastun wkrótce dostępny!");
+    playAlert();
+}
+function playAlert() {
+    Swal.fire({
+        title: 'Brak dostępu',
+        text: 'Zwiastun dostępny za '+daysUntil("2025-05-05")+'.',
+        icon: 'error',
+        confirmButtonText: 'OK',
+    });
+}
+function daysUntil(targetDate) {
+    const currentDate = new Date();
+    const endDate = new Date(targetDate);
+    const timeDifference = endDate - currentDate;
+    const daysDifference = Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+    return `${daysDifference} dni`;
 }
